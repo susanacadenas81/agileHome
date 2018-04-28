5966926f4331fb105a14e449bc6d2bf0f4e56767
susanacadenas81 <susanacadenasgomez81@gmail.com>
2018-04-28 14:28:26 +0200

    Componentes creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './componentes/home-page/home-page.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { RegisterComponent } from './componentes/register/register.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { PrivadoComponent } from './componentes/privado/privado.component';
+import { PrivateComponent } from './componentes/private/private.component';
+import { NotFoundComponent } from './componentes/not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavbarComponent,
+    RegisterComponent,
+    LoginComponent,
+    PrivadoComponent,
+    PrivateComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
